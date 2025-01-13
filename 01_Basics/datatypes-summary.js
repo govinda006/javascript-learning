23ba9d90fd42f776a7749b40bb1831186a2c39d8
Govinda Prasad Parida <62826820+govinda006@users.noreply.github.com>
2025-01-13 17:10:10 +0000

    code changed

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,26 @@
+//# Primitive // get the exact value from the memory
+//7 types: String,Number,Null,Boolean,Undefined,Symbol,BigInt
+
+const score = true;
+console.log(typeof score);
+
+const isLoggedIn = true;
+console.log(isLoggedIn);
+
+let userEmail;
+
+//Referance type // get the referance of the value from memory
+//Array , Objects , Functions
+
+const heros = ["shaktiman","krish","flying jatt"]
+// {
+//     name: "Govinda",
+//     age: 22,
+// }
+
+const myFunction = function(){
+console.log("HELLO");
+
+}
+
+myFunction();

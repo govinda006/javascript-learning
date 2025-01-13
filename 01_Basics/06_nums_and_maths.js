dad9e77731a2debd35c46c1507cf9b9c8e27d266
Govinda Prasad Parida <62826820+govinda006@users.noreply.github.com>
2025-01-13 22:08:38 +0000

    code updated

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,28 @@
+const score = 400;
+
+const balance = new Number(100);
+// console.log(balance);
+// console.log(score);
+
+// console.log(balance.toString().length);
+
+const otherNumber = 23.5933;
+
+// console.log(otherNumber.toPrecision(2));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ++++++++++++++++++ MATHS +++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(34.5432));
+
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);

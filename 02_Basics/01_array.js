a30fbabd85a5b482a40f73a4987958358812af25
Govinda Prasad Parida <62826820+govinda006@users.noreply.github.com>
2025-01-14 18:26:28 +0000

    code updated

--- 02_Basics/01_array.js
+++ 02_Basics/01_array.js
@@ -4,7 +4,7 @@ const myArr = [0, 1, 2, 3, 4, 5];
 
 const myArr2 = new Array(1, 2, 3, 4);
 
-console.log(myArr2);
+// console.log(myArr2);
 // Array methods
 
 // myArr.push(6);
@@ -30,5 +30,10 @@ console.log("A", myArr);
 
 const myn1 = myArr.slice(1, 3);
 
-console.log(myn1);
+console.log("slice-", myn1);
 console.log("B", myArr);
+
+const myn2 = myArr.splice(1, 3);
+console.log("splice-", myn2);
+
+console.log("C-originalArray-", myArr);

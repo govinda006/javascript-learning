b603a6b96969a7283947eff810b598732166f33c
Govinda Prasad Parida <62826820+govinda006@users.noreply.github.com>
2025-01-14 18:08:41 +0000

    code changed

---
+++ 02_Basics/01_array.js
@@ -0,0 +1,15 @@
+//array
+
+const myArr = [0, 1, 2, 3, 4, 5];
+
+const myArr2 = new Array(1, 2, 3, 4);
+
+console.log(myArr2);
+// Array methods
+
+// myArr.push(6);
+// myArr.push(7);
+
+myArr.unshift(8);
+
+console.log(myArr);

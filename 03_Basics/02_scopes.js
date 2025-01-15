32da7e1f26c4165dcc75d2d8e2f548badec98dc2
Govinda Prasad Parida <62826820+govinda006@users.noreply.github.com>
2025-01-15 19:28:12 +0000

    code updated

---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,32 @@
+// // let a = 10;
+// // const b = 20;
+// // var c = 30;
+
+// // console.log(a);
+// // console.log(b);
+// // console.log(c);
+
+// function one() {
+//   const username = "Govinda";
+
+//   function two() {
+//     const website = "Gp website";
+//     console.log(username);
+//   }
+//   //   console.log(website);
+//   two();
+// }
+// one();
+
+// +++++++++++++++++++ intreseting +++++++++++++++++
+
+function addone(num) {
+  return num + 1;
+}
+console.log(addone(5));
+
+const addTwo = function (num) {
+  return num + 2;
+};
+
+console.log(addTwo(5));

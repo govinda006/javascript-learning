79c75b9c0b0b97221bacde09c5827ccaf18edc0a
Govinda Prasad Parida <62826820+govinda006@users.noreply.github.com>
2025-01-15 21:16:51 +0000

    code added

---
+++ 04_controll-flow/one.js
@@ -0,0 +1,14 @@
+//if
+
+const isUserLoggedIn = true;
+
+if (2 != 3) {
+  // code execute
+  console.log("True");
+} else {
+  console.log("Not the same datetypes");
+}
+
+if (false) {
+  //code doesn't execute
+}

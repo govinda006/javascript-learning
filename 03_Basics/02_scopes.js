f8ead105192a86e5c714d30b81d892e5d2745e17
Govinda Prasad Parida <62826820+govinda006@users.noreply.github.com>
2025-01-15 19:41:01 +0000

    code updated

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -20,13 +20,13 @@
 
 // +++++++++++++++++++ intreseting +++++++++++++++++
 
-function addone(num) {
-  return num + 1;
-}
-console.log(addone(5));
+// console.log(addone(5));
 
-const addTwo = function (num) {
-  return num + 2;
-};
+// function addone(num) {
+//   return num + 1;
+// }
 
-console.log(addTwo(5));
+// console.log(addTwo(5));
+// const addTwo = function (num) {
+//   return num + 2;
+// };

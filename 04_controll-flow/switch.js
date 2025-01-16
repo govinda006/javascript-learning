0929bf029023e9bc375c9b566bd39faf76087792
Govinda Prasad Parida <62826820+govinda006@users.noreply.github.com>
2025-01-16 11:32:34 +0000

    code added

---
+++ 04_controll-flow/switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march";
+
+switch (month) {
+  case "jan":
+    console.log("januyary");
+    break;
+  case "feb":
+    console.log("feb");
+    break;
+  case "march":
+    console.log("March");
+    break;
+  case "april":
+    console.log("April");
+    break;
+
+  default:
+    console.log("default case match");
+    break;
+}
+ 

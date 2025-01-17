b4d8d97303b2242cd5a9ed70ba664bea513305f9
Govinda Prasad Parida <62826820+govinda006@users.noreply.github.com>
2025-01-17 14:54:14 +0000

    code added

--- 04_controll-flow/truthy-falsy.js
+++ 04_controll-flow/truthy-falsy.js
@@ -1,11 +1,17 @@
-const userEmail = "";
+// const userEmail = "";
 
-if (userEmail) {
-  console.log("Got user email");
-} else {
-  console.log("Don't have user email");
-}
+// if (userEmail) {
+//   //   console.log("Got user email");
+// } else {
+//   //   console.log("Don't have user email");
+// }
 
-//falsy values - false, 0,-0,BigInt(0n),"",null,undefined,Nan
+// //falsy values - false, 0,-0,BigInt(0n),"",null,undefined,Nan
 
-//truthy values - "0", "false", " ",[],{},function(){}
+// //truthy values - "0", "false", " ",[],{},function(){}
+
+// if (userEmail === 0) {
+//   console.log(typeof userEmail);
+// }
+
+

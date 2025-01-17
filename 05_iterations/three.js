03d25371894be2a8888566cd11466b8d45a6bda5
govinda006 <govindaprasadparida2@gmail.com>
2025-01-18 00:36:57 +0530

    add four.js and update three.js with commented examples for iteration methods

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -1,41 +1,41 @@
-// // for of
-// ["", "", ""];
-// [({}, {}, {})];
+// // // for of
+// // ["", "", ""];
+// // [({}, {}, {})];
 
-const arr = [1, 2, 3, 4, 5];
+// const arr = [1, 2, 3, 4, 5];
 
-for (const num of arr) {
-  //   console.log(num);
-}
-
-const greetings = "Hello World!";
-for (const greet of greetings) {
-  //   console.log(`Each char is ${greet}`);
-}
-
-//Maps - takes only unique values
-
-const map = new Map();
-map.set("IN", "India");
-map.set("USA", "United States Of America");
-map.set("Fr", "France");
-
-// console.log(map);
-
-for (const [key, value] of map) {
-  console.log(`${key} :- ${value}`);
-}
-
-const myObj = {
-  game1: "NFS",
-  game2: "GTA",
-  game3: "CSGO",
-};
-
-// not workable for myObj
-
-// for (const [key, value] of myObj) {
-//   console.log(`${key} :- ${value}`);
-//   const games = new myObj();
-//   console.log(games);
+// for (const num of arr) {
+//   //   console.log(num);
 // }
+
+// const greetings = "Hello World!";
+// for (const greet of greetings) {
+//   //   console.log(`Each char is ${greet}`);
+// }
+
+// //Maps - takes only unique values
+
+// const map = new Map();
+// map.set("IN", "India");
+// map.set("USA", "United States Of America");
+// map.set("Fr", "France");
+
+// // console.log(map);
+
+// for (const [key, value] of map) {
+//   console.log(`${key} :- ${value}`);
+// }
+
+// const myObj = {
+//   game1: "NFS",
+//   game2: "GTA",
+//   game3: "CSGO",
+// };
+
+// // not workable for myObj
+
+// // for (const [key, value] of myObj) {
+// //   console.log(`${key} :- ${value}`);
+// //   const games = new myObj();
+// //   console.log(games);
+// // }

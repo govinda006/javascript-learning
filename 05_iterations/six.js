de0ad6898552b9b5a31288de483a421651a26962
govinda006 <govindaprasadparida2@gmail.com>
2025-01-18 02:31:58 +0530

    code changed

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -1,103 +1,103 @@
-// const coding = ["js", "ruby", "java", "python", "cpp"];
+// // const coding = ["js", "ruby", "java", "python", "cpp"];
 
-// // const values = coding.forEach((item) => {
-// //   console.log(item);
+// // // const values = coding.forEach((item) => {
+// // //   console.log(item);
 
-// // });
+// // // });
 
-// // console.log(values);
+// // // console.log(values);
 
-// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// // const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// // const newNums = myNums.filter((num) => {
-// //   return num > 4;
-// // })
+// // // const newNums = myNums.filter((num) => {
+// // //   return num > 4;
+// // // })
 
-// const newNums = [];
+// // const newNums = [];
 
-// // myNums.forEach((num) => {
-// //   if (num > 4) {
-// //     newNums.push(num);
-// //   }
-// // });
+// // // myNums.forEach((num) => {
+// // //   if (num > 4) {
+// // //     newNums.push(num);
+// // //   }
+// // // });
 
-// console.log(newNums);
+// // console.log(newNums);
 
-const books = [
-  {
-    name: "Book 1",
-    description: "An intriguing tale of mystery and adventure.",
-    genre: "Mystery",
-    publish: "2020",
-    edition: "First",
-  },
-  {
-    name: "Book 2",
-    description: "A deep dive into the world of science fiction.",
-    genre: "Science Fiction",
-    publish: "2018",
-    edition: "Second",
-  },
-  {
-    name: "Book 3",
-    description: "A heartwarming story of love and friendship.",
-    genre: "Romance",
-    publish: "2019",
-    edition: "Third",
-  },
-  {
-    name: "Book 4",
-    description: "A thrilling narrative of crime and justice.",
-    genre: "Mystery",
-    publish: "2021",
-    edition: "First",
-  },
-  {
-    name: "Book 5",
-    description: "A comprehensive guide to programming and coding.",
-    genre: "Technical",
-    publish: "2017",
-    edition: "Fourth",
-  },
-  {
-    name: "Book 6",
-    description: "An inspiring autobiography of a great leader.",
-    genre: "Biography",
-    publish: "2016",
-    edition: "First",
-  },
-  {
-    name: "Book 7",
-    description: "A detailed exploration of ancient history.",
-    genre: "History",
-    publish: "2015",
-    edition: "Second",
-  },
-  {
-    name: "Book 8",
-    description: "A collection of captivating short stories.",
-    genre: "Fiction",
-    publish: "2020",
-    edition: "Third",
-  },
-  {
-    name: "Book 9",
-    description: "Another mystery that keeps you guessing till the end.",
-    genre: "Mystery",
-    publish: "2018",
-    edition: "First",
-  },
-  {
-    name: "Book 10",
-    description: "An analysis of the modern economic landscape.",
-    genre: "Economics",
-    publish: "2022",
-    edition: "First",
-  },
-];
+// const books = [
+//   {
+//     name: "Book 1",
+//     description: "An intriguing tale of mystery and adventure.",
+//     genre: "Mystery",
+//     publish: "2020",
+//     edition: "First",
+//   },
+//   {
+//     name: "Book 2",
+//     description: "A deep dive into the world of science fiction.",
+//     genre: "Science Fiction",
+//     publish: "2018",
+//     edition: "Second",
+//   },
+//   {
+//     name: "Book 3",
+//     description: "A heartwarming story of love and friendship.",
+//     genre: "Romance",
+//     publish: "2019",
+//     edition: "Third",
+//   },
+//   {
+//     name: "Book 4",
+//     description: "A thrilling narrative of crime and justice.",
+//     genre: "Mystery",
+//     publish: "2021",
+//     edition: "First",
+//   },
+//   {
+//     name: "Book 5",
+//     description: "A comprehensive guide to programming and coding.",
+//     genre: "Technical",
+//     publish: "2017",
+//     edition: "Fourth",
+//   },
+//   {
+//     name: "Book 6",
+//     description: "An inspiring autobiography of a great leader.",
+//     genre: "Biography",
+//     publish: "2016",
+//     edition: "First",
+//   },
+//   {
+//     name: "Book 7",
+//     description: "A detailed exploration of ancient history.",
+//     genre: "History",
+//     publish: "2015",
+//     edition: "Second",
+//   },
+//   {
+//     name: "Book 8",
+//     description: "A collection of captivating short stories.",
+//     genre: "Fiction",
+//     publish: "2020",
+//     edition: "Third",
+//   },
+//   {
+//     name: "Book 9",
+//     description: "Another mystery that keeps you guessing till the end.",
+//     genre: "Mystery",
+//     publish: "2018",
+//     edition: "First",
+//   },
+//   {
+//     name: "Book 10",
+//     description: "An analysis of the modern economic landscape.",
+//     genre: "Economics",
+//     publish: "2022",
+//     edition: "First",
+//   },
+// ];
 
-let userBooks = books.filter((bks) => bks.genre === "Mystery");
-userBooks = books.filter((bks) => {
-  return bks.publish >= 2015 && bks.genre === "Mystery";
-});
-console.log(userBooks);
+// let userBooks = books.filter((bks) => bks.genre === "Mystery");
+// userBooks = books.filter((bks) => {
+//   return bks.publish >= 2015 && bks.genre === "Mystery";
+// });
+// console.log(userBooks);

676329ac7e67b8ba505ba56f73a3d4e9bc2a4547
govinda006 <govindaprasadparida2@gmail.com>
2025-01-18 19:54:46 +0530

    final code added

--- projects/chaiaurcode.js
+++ projects/chaiaurcode.js
@@ -1,4 +1,4 @@
-const randomNumber = parseInt(Math.random() * 100 + 1);
+let randomNumber = parseInt(Math.random() * 100 + 1);
 const submit = document.querySelector("#subt");
 const userInput = document.querySelector("#guessField"); // Fixed typo
 const guessSlot = document.querySelector(".guesses");
@@ -66,8 +66,24 @@ function displayMessage(message) {
 }
 
 function endGame() {
-  //
+  userInput.value = "";
+  userInput.setAttribute("disabled", "");
+  p.classList.add("button");
+  p.innerHTML = `<h2 id="newGame">Start new game</h2>`;
+  startOver.appendChild(p);
+  playGame = false;
+  newGame();
 }
 function newGame() {
-  //
+  const newGameButton = document.querySelector("#newGame");
+  newGameButton.addEventListener("click", function (e) {
+    randomNumber = parseInt(Math.random() * 100 + 1);
+    prevGuess = [];
+    numGuess = 1;
+    guessSlot.innerHTML = "";
+    remaining.innerHTML = `${11 - numGuess}`;
+    userInput.removeAttribute("disabled");
+    startOver.removeChild(p);
+    playGame = true;
+  });
 }
